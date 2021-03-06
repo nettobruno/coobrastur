1a9b77f6a3acc53e75ecdd16681c936921c0933b
Bruno Netto <brunonetto@MacBook-Air-de-Bruno.local>
2021-03-05 23:16:02 -0300

    Adding home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,97 @@
-import React from 'react';
+// Dependencies
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
-import { Container } from './styles';
+// Images
+import logo from '../../assets/logo-grey.svg';
+import logout from '../../assets/logout.svg';
+import user from '../../assets/user.svg';
+import add from '../../assets/add.svg';
+
+// Styles
+import {
+  Container,
+  Header,
+  ContentUsers,
+  BlockUser,
+  Total,
+  Buttons,
+  Button,
+} from './styles';
 
 function Home() {
+  const [page, setPage] = useState(1);
+  const [users, setUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(null);
+  const history = useHistory();
+
+  async function getAllUsers() {
+    const response = await api.get(`/api/users?page=${page}`);
+    setUsers(response.data.data);
+    setTotalUsers(response.data.total);
+  }
+
+  useEffect(() => {
+    getAllUsers();
+  }, [page]);
+
   return (
-    <Container>
-      <h1>Home page</h1>
-    </Container>
+    <>
+      <Header>
+        <div className="container">
+          <img src={logo} alt="" />
+
+          <div>
+            <button type="button" onClick={() => history.push('/addclient')}>
+              <img src={add} alt="" />
+              <p>Novo Cliente</p>
+            </button>
+
+            <div className="block-logout">
+              <img src={logout} alt="" />
+              <p>Sair</p>
+            </div>
+          </div>
+        </div>
+      </Header>
+
+      <Container className="container">
+        <h1>
+          <img src={user} alt="" />
+          Painel de Clientes
+        </h1>
+
+        {users && (
+          <ContentUsers>
+            {users.map((u) => (
+              <BlockUser key={u.id}>
+                <img src={u.avatar} alt="" />
+                <p className="name">
+                  {u.first_name} {u.last_name}
+                </p>
+                <p>{u.email}</p>
+
+                <Button type="button">Editar</Button>
+              </BlockUser>
+            ))}
+          </ContentUsers>
+        )}
+
+        <Total>
+          <p>Total: {totalUsers} clientes</p>
+        </Total>
+
+        <Buttons>
+          <Button type="button" onClick={() => setPage(page - 1)}>
+            Anterior
+          </Button>
+          <Button type="button" onClick={() => setPage(page + 1)}>
+            Próximo
+          </Button>
+        </Buttons>
+      </Container>
+    </>
   );
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,162 @@
 import styled from 'styled-components';
 
+export const Header = styled.div`
+  background-color: #08436f;
+  margin-bottom: 100px;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  button {
+    border-radius: 9px;
+    background-color: #f8fcff;
+    color: #534c4c;
+
+    text-transform: uppercase;
+
+    padding: 10px 20px;
+    margin-right: 20px;
+
+    cursor: pointer;
+
+    img {
+      margin-right: 10px;
+    }
+  }
+
+  .block-logout p {
+    color: white;
+    margin-left: 5px;
+  }
+
+  @media (max-width: 600px) {
+    button {
+      padding: 10px;
+      margin-right: 10px;
+
+      position: absolute;
+      top: 20px;
+      left: 10px;
+
+      img {
+        margin-right: 0px;
+      }
+
+      p {
+        display: none;
+      }
+    }
+
+    .block-logout img {
+      position: absolute;
+      top: 30px;
+      right: 10px;
+      width: 20px;
+    }
+
+    .block-logout p {
+      display: none;
+    }
+  }
+`;
+
 export const Container = styled.div`
-  border: 1px solid red;
+  h1 {
+    color: #08436f;
+    font-size: 22px;
+    border-bottom: 1px solid #e9e9e9;
+    padding-bottom: 10px;
+  }
+
+  h1 img {
+    margin-right: 10px;
+  }
+`;
+
+export const ContentUsers = styled.div`
+  margin-top: 20px;
+`;
+
+export const BlockUser = styled.div`
+  border: 1px solid #e5e5e5;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: 10px;
+  padding: 10px;
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+  }
+
+  p {
+    font-size: 17px;
+  }
+
+  p.name {
+    color: #08436f;
+  }
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+
+    img {
+      width: 100px;
+      height: 100px;
+      border-radius: 0px;
+      margin-bottom: 10px;
+    }
+
+    p {
+      font-size: 17px;
+      margin-bottom: 10px;
+    }
+
+    p.name {
+      color: #08436f;
+      font-weight: bold;
+    }
+  }
+`;
+
+export const Total = styled.div`
+  color: #534c4c;
+  font-size: 17px;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 300px;
+  margin: 50px auto;
+`;
+
+export const Button = styled.button`
+  border: 1px solid #08436f;
+  background-color: white;
+  border-radius: 5px;
+  color: #08436f;
+
+  font-size: 14px;
+
+  padding: 12px;
+  margin-left: 5px;
+
+  cursor: pointer;
+
+  transition: 0.2s;
+
+  :hover {
+    background-color: #08436f;
+    color: white;
+  }
 `;
